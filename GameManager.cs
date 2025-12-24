bd7d8f650d7a89294d37cbebbe6c5057309f0a91
Haste007 <umairfaisal645@gmail.com>
2025-12-24 19:08:55 +0500

    New TicTacToe

---
+++ GameManager.cs
@@ -0,0 +1,162 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace TicTacToe;
+
+public class GameManager
+{
+    public int BoardSize { get; set; } = 3;
+    public  BoardBox[][] Board {get; set;}
+    public void PrintBoard()
+    {
+        if (!Console.IsOutputRedirected)
+        {
+            Console.Clear();
+        }
+        System.Console.WriteLine("------------Tic Tac Toe---------------------");
+        for(int i = 0; i< BoardSize; i++)
+        {
+            Console.Write("|");
+            for(int j= 0; j<BoardSize; j ++)
+            {
+                var box = Board[i][j];
+                switch (box)
+                {
+                    case BoardBox.None:
+                        int boxNo = (i*BoardSize)+j;
+                        string printed = boxNo < 10? $"0{boxNo}":$"{boxNo}";
+                        System.Console.Write($"| {boxNo} |");
+                        break;
+                    case BoardBox.Player1:
+                        System.Console.Write($"| P1 |");
+                        break;
+                    case BoardBox.Player2:
+                        System.Console.Write($"| P2 |");
+                        break;
+                }
+            }
+            Console.Write("|\n");
+        }
+        System.Console.WriteLine("----------------------------------------");
+    }
+    public bool Player1Turn {get; set;}
+    public bool Player2Turn => !Player1Turn;
+    public BoardBox Winner;
+
+    public bool GameEnded => Winner != BoardBox.None;
+
+    public void TakeTurn(int choice)
+    {
+        TakeTurn((choice)%BoardSize , (choice)/3);
+    }
+
+    public void TakeTurn(int col,int row)
+    {
+        if(col >= BoardSize || row >= BoardSize) return;
+        if(GameEnded) return;
+
+        BoardBox selected = Board[row][col];
+        if(selected != BoardBox.None) return;
+        Board[row][col] = Player1Turn? BoardBox.Player1 : BoardBox.Player2;
+        CheckWin(Board[row][col]);
+        CheckDraw();
+
+        if(Winner == BoardBox.None)
+        Player1Turn = !Player1Turn;
+    }
+
+    private void CheckDraw()
+    {
+        bool BoardFilled = Board.All(row => row.All(x => x != BoardBox.None));
+        if(BoardFilled && Winner == BoardBox.None)
+            Winner = BoardBox.Draw;
+    }
+
+    private void SetBoard()
+    {
+        Board = [[BoardBox.None,BoardBox.None,BoardBox.None],
+                [BoardBox.None,BoardBox.None,BoardBox.None],
+                [BoardBox.None,BoardBox.None,BoardBox.None]];
+        Player1Turn = true;
+        Winner = BoardBox.None;
+    }
+
+    public GameManager()
+    {
+        Board = new BoardBox[3][];
+        SetBoard();
+    }
+
+    public void ResetGame()
+    {
+        SetBoard();
+    }
+
+    public void CheckWin(BoardBox player)
+    {
+        if(player is BoardBox.None) return;
+        //Check Win Conditions
+        bool isHorizontal = false;
+        bool isVertical = false;
+        bool isDiagonal = false;
+
+        //Check Horizontal
+        for(int i=0; i<BoardSize; i++)
+        {
+            isHorizontal = true;
+            for(int j=0; j< BoardSize; j++)
+            {
+                if(Board[i][j] == player) continue;
+                isHorizontal = false;
+                break;
+            };
+            if(isHorizontal) break;
+        }
+
+        //Check for vertical
+        // check each column,
+            // set found to true, if a box is bad, set found to false, if by the end the found is true, return found
+
+
+        for(int i=0; i< BoardSize; i++)
+        {
+            isVertical = true;
+            for(int j=0; j< BoardSize; j++)
+            {
+                if(Board[j][i] == player) continue;
+                isVertical = false;
+                break;
+            };
+            if(isVertical) break;
+        }
+
+        //Check for diagonals
+        //Main Diagonal
+        for(int i=0; i< BoardSize; i++)
+        {
+            isDiagonal = true;
+            if(Board[i][i] == player) continue;
+            isDiagonal = false;
+            break;
+        }
+
+        //Anti Diagonal
+        for(int i=0; i< BoardSize; i++)
+        {
+            isDiagonal = true;
+            if(Board[i][BoardSize-i-1] == player) continue;
+            isDiagonal = false;
+            break;
+        }
+
+        if(isDiagonal || isHorizontal || isVertical) Winner = player;
+        return;
+    }
+}
+
+public enum BoardBox
+{
+    None,
+    Player1,
+    Player2,
+    Draw
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using TicTacToe;
+GameManager game = new();
+
+while(game.Winner == BoardBox.None)
+{
+
+    game.PrintBoard();
+    string turnOwner = game.Player1Turn? "Player 1" : "Player 2";
+    System.Console.WriteLine($"Current Turn: {turnOwner}");
+    Console.Write("Take Turn:");
+    var c = Console.ReadLine();
+    if(int.TryParse(c,out int result))
+    {
+        game.TakeTurn(result);
+
+        if(game.Winner != BoardBox.None)
+        {
+            game.PrintBoard();
+            System.Console.WriteLine($"Game Won by {turnOwner}\n");
+
+            Console.WriteLine($"Press R to reset, Press E to end");
+            char[] validInputs = ['r','R','e','E'];
+            char order = '\0';
+            while(!validInputs.Contains(order))
+            {
+            var key = Console.ReadKey();
+            order = key.KeyChar;
+            }
+            switch (order)
+            {
+                case 'r':
+                case 'R':
+                    game.ResetGame();
+                    break;
+                case 'e':
+                case 'E':
+                    break;
+            }
+
+        }
+    }
+}
